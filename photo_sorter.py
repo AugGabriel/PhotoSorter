d0446b4525239e77af7c81f4e4b1c4259cf98076
GabrielAugusto <gabriel.galup@gmail.com>
2026-06-16 10:20:40 -0300

    Alterações e testes nas funções implementadas do projeto

--- photo_sorter.py
+++ photo_sorter.py
@@ -10,6 +10,10 @@ PATH = r"C:\Users\gabri_06sjdhx\OneDrive\Imagens\Neural"
 DIRS = os.scandir(PATH)
 
 def main():
+    # [print(get_archv_date(archive)) for archive in DIRS]
+    print(older(DIRS))
+
+    '''
     try:
         open(PATH)
     except PermissionError as e:
@@ -20,17 +24,40 @@ def main():
     
     for d in range(datetime.datetime.now().year, older(DIRS) - 1, -1):
         print(d)
+    '''
 
 
-def older(dirs):
-    minimum = min([get_archv_date(archive).year for archive in dirs])
-    return minimum
+"""
+Returns the year of the older archive in directory
+"""
+def older(root_dir):
+    # Initializes variable
+    min_year = -1
 
-    
+    for archive in root_dir:
+        # If is an archive, compares its year with the minimum year so far
+        if not os.path.isdir(archive): 
+            if min_year < 0:
+                min_year = get_archv_date(archive).year
+            else:
+                min_year = min(min_year, get_archv_date(archive).year)
+
+        # If is a directory, recursively gets the older archive inside and compares with the variable
+        else:
+            if min_year < 0:
+                min_year = older(archive)
+            else:
+                min_year = min(min_year, older(archive))
+
+    return min_year
+
+
+"""
+Gets the creation date according to the operational system (which may not be the real one),
+and tries to get the date in the name of the archive, which is usually the correct one. 
+Then, uses the minimum between both
+"""
 def get_archv_date(archive: os.DirEntry):
-    # Gets the creation date according to the operational system (which may not be the real one),
-    # and tries to get the date in the name of the archive, which is usually the correct one
-
     t1 = time.localtime(os.path.getctime(archive))
     d1 = datetime.date(year=t1.tm_year, month=t1.tm_mon, day=t1.tm_mday)
     
@@ -39,10 +66,13 @@ def get_archv_date(archive: os.DirEntry):
     return min(d1, d2) if d2 else d1
 
 
+"""
+Returns the date according to the name of the archive
+"""
 def date_by_name(archive):
     match = re.search(r'[0-9]{4}-[0-9]{2}-[0-9]{2}', archive.name)
     if (match):
-        m = (match.string)[0:10]
+        m = (match.string)[match.start():match.end()]
         d = datetime.date(year=int(m[0:4]), month=int(m[5:7]), day=int(m[8:10]))
         return d
     
